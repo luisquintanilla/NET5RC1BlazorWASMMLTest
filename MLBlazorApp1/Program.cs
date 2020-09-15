f1e68834cb3dd7ce63ed891efad536da0ce052e1
luisquintanilla <Luis.Quintanilla@microsoft.com>
2020-09-14 21:55:11 -0400

    Updated with model

--- MLBlazorApp1/Program.cs
+++ MLBlazorApp1/Program.cs
@@ -16,7 +16,7 @@ namespace MLBlazorApp1
         {
             var builder = WebAssemblyHostBuilder.CreateDefault(args);
             builder.RootComponents.Add<App>("app");
-
+            
             builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
             await builder.Build().RunAsync();
